dd18b54c797062da020bad34f2c3fe7d55f69b15
Thibault Théologien <thibault@nobbas.com>
2019-09-23 17:22:02 -0400

    Update  filterValidator function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import * as Joi from '@hapi/joi';
 
+/**
+ * TODO: Migrate to another library more generic;
+ * - has to validate that the response contains next and previous page related keys when using pagination
+ */
 export module QueryParamValidator {
-  export const filterValidator = (type: 'number' | 'string') =>
-    Joi.object({
+  export const filterValidator = (type: 'number' | 'string' | 'boolean') => {
+    const defaultFilters = Joi.object({
       eq: Joi[type](),
       ne: Joi[type](),
-      gt: Joi[type](),
-      gte: Joi[type](),
-      lt: Joi[type](),
-      lte: Joi[type](),
       sortBy: Joi.string().valid('asc', 'desc'),
     });
+
+    return type === 'number'
+      ? defaultFilters.concat(
+          Joi.object({
+            gt: Joi[type](),
+            gte: Joi[type](),
+            lt: Joi[type](),
+            lte: Joi[type](),
+          }),
+        )
+      : defaultFilters;
+  };
 
   export const paginationValidator = Joi.object({
     page: Joi.number()
